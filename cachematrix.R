4be954c4610065c05810ba9d65862fdf74bc04f6
erikschakenbos <erik.schakenbos@gmail.com>
2015-06-20 11:37:23 +0200

    2015-06-20 11:37

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+#===========================================
+#Autor:     Erik Schakenbos
+#Date:      2015-06-20
+#Description:
+#Script contains 2 functions which are able
+#to interact. 
+#
+#makeCachMatrix stores input and
+#inverted matrix to environment objects.
+#
+#cacheSolve compares input matrix with stored
+#environment matrix. If different, call makeCacheMatrix
+#if the same, return stored env object of invert
+#===========================================
 
-makeCacheMatrix <- function(x = matrix()) {
+#===========================================
+#makeCacheMatrix:
+#Store original matrix for identical compare 
+#Store invert
+#===========================================
+makeCacheMatrix <- function(x = matrix()) 
+    {
+        orgM <<- x
+        invM <<- solve(x)
+    }
 
-}
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+#===========================================
+#cacheSolve:
+#Checks if input matrix is identical with one env.
+#If yes, then get cached invert form env
+#If no, then call makeCacheMatrix
+#===========================================
+cacheSolve <- function(x, ...) 
+    {
+        if(identical(x, orgM))    
+            {
+                #If x and orgM are identical, then output is invM
+                message("input = cached org M, retrieving invM")
+                return(invM)
+            }
+        makeCacheMatrix(x)
+        return(invM)
+    }
